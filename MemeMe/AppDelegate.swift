3a225d54d85163d4b5bb8576c943e5bdb1520c5f
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2021-07-18 00:41:41 +0700

    Remove swiftlint warning

--- MemeMe/AppDelegate.swift
+++ MemeMe/AppDelegate.swift
@@ -15,8 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 	var memes = [Meme]()
 
-	func application(_ application: UIApplication,
-					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		return true
 	}
