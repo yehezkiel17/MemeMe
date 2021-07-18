5755d4fbc19c00bf942a56bf699c68dec06a4623
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2021-07-19 02:36:44 +0700

    Add MemeTableViewCell

---
+++ MemeMe/Views/MemeTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMe
+//
+//  Created by Yehezkiel Litbagay on 19/07/21.
+//  Copyright © 2021 Yehezkiel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+	
+}
