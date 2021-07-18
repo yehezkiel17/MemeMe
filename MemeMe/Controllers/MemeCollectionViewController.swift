62574b250482d3577d9e76aa89ec4083ad84daf1
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2021-07-18 21:36:02 +0700

    Create MemeTableViewController & MemeCollectionViewController

---
+++ MemeMe/Controllers/MemeCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MemeCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Yehezkiel Litbagay on 18/07/21.
+//  Copyright © 2021 Yehezkiel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeCollectionViewController: UICollectionViewController {
+	
+}
---
+++ MemeMe/Controllers/MemeTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MemeTableViewController.swift
+//  MemeMe
+//
+//  Created by Yehezkiel Litbagay on 18/07/21.
+//  Copyright © 2021 Yehezkiel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTableViewController: UITableViewController {
+	
+}
