af6baec13772db64afac13fcb80907fb3027e695
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2021-07-19 01:47:23 +0700

    Set the flow layout

--- MemeMe/Controllers/MemeCollectionViewController.swift
+++ MemeMe/Controllers/MemeCollectionViewController.swift
@@ -11,19 +11,48 @@ import UIKit
 
 class MemeCollectionViewController: UICollectionViewController {
 	
-	var memes: [Meme] {
+	//MARK: -Outlets
+	@IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+	
+	// MARK: -Variables/Constants
+	private var memes: [Meme] {
 		return getSharedMemes()
 	}
 	
+	//MARK: -Lifecycle Methods
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		setupFlowLayout()
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		collectionView.reloadData()
+	}
+	
+	//MARK: -Data Source
 	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 0
+		return memes.count
 	}
 	
 	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		return UICollectionViewCell()
 	}
 	
+	//MARK: -Delegate
 	override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 		
+	}
+	
+	// MARK: -Private methods
+	private func setupFlowLayout() {
+		let space:CGFloat = 3.0
+		let dimension = (view.frame.size.width - (2 * space)) / 3.0
+		
+		flowLayout.minimumInteritemSpacing = space
+		flowLayout.minimumLineSpacing = space
+		flowLayout.itemSize = CGSize(width: dimension, height: dimension)
 	}
 }
