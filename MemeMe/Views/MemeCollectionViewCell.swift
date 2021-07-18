0d450277caab0310963ab469ef57843422991c78
Yehezkiel Litbagay <kaka.yehezkiel@gmail.com>
2021-07-19 01:46:30 +0700

    Add MemeCollectionViewCell

---
+++ MemeMe/Views/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Yehezkiel Litbagay on 19/07/21.
+//  Copyright © 2021 Yehezkiel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+	
+	@IBOutlet weak var imageView: UIImageView!
+}
